d8e46b6bced36def822c7048afd36c8f83901ea0
Max van Deurzen <mi.vandeurzen@gmail.com>
2025-11-23 21:26:57 +0100

    delete

--- src/fetcher.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios"
-import { BASE_URL, KEY } from "./constants";
-
-axios.defaults.baseURL = BASE_URL;
-axios.defaults.headers.common['Authorization'] = KEY;
-// axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
-
-export default axios
